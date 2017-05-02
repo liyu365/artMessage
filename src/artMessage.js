cd8773883eadb5be48b2863eb82415e70623107a
liyu <webcitizen@163.com>
2017-05-02 17:04:58 +0800

    modify

--- src/artMessage.js
+++ src/artMessage.js
@@ -21,22 +21,24 @@
         }
 
         var type = document.createElement('i');
-        switch (_this.type){
+        switch (_this.type) {
             case 'success':
-                addClass(type,'success');
+                addClass(type, 'success');
                 break;
             case 'error':
-                addClass(type,'error');
+                addClass(type, 'error');
                 break;
             case 'warn':
-                addClass(type,'warn');
+                addClass(type, 'warn');
                 break;
             case 'info':
-                addClass(type,'info');
+                addClass(type, 'info');
                 break;
             case 'loading':
-                addClass(type,'loading');
+                addClass(type, 'loading');
                 break;
+            default :
+                type.style.display = 'none';
         }
 
         var custom_msg = document.createElement('span');
@@ -60,9 +62,11 @@
             removeClass(_this.messageItem, 'move-up-enter-active');
         }, 200);
 
-        setTimeout(function () {
-            _this.out();
-        }, _this.duration * 1000);
+        if (_this.duration !== 0) {
+            setTimeout(function () {
+                _this.out();
+            }, _this.duration * 1000);
+        }
     };
 
     Message.prototype.out = function () {
@@ -97,68 +101,6 @@
      * utils----------------------------------------------------------------------------------
      */
 
-    //根据属性名获取元素集合
-    function getElementsByAttribute(attribute, attributeValue, queryElement) {
-        var elementArray = [];
-        var matchedArray = [];
-        var qElement = document;
-        if (typeof queryElement !== 'undefined') {
-            qElement = queryElement;
-        }
-        if (qElement.all) {
-            elementArray = qElement.all;
-        } else {
-            elementArray = qElement.getElementsByTagName("*");
-        }
-        for (var i = 0, len = elementArray.length; i < len; i++) {
-            if (attribute == "class") {
-                var pattern = new RegExp("(\\s|^)" + attributeValue + "(\\s|$)");
-                if (pattern.test(elementArray[i].className)) {
-                    matchedArray[matchedArray.length] = elementArray[i];
-                }
-            } else if (attribute == "for") {
-                if (elementArray[i].getAttribute("htmlFor") || elementArray[i].getAttribute("for")) {
-                    if (elementArray[i].htmlFor == attributeValue) {
-                        matchedArray[matchedArray.length] = elementArray[i];
-                    }
-                }
-            } else if (elementArray[i].getAttribute(attribute) == attributeValue) {
-                matchedArray[matchedArray.length] = elementArray[i];
-            }
-        }
-
-        return matchedArray;
-    }
-
-    //事件监听
-    function attachEventListener(target, eventType, handler) {
-        if (typeof target.addEventListener != "undefined") {
-            target.addEventListener(eventType, handler, false);
-        } else if (typeof target.attachEvent != "undefined") {
-            target.attachEvent("on" + eventType, handler);
-        } else {
-            target["on" + eventType] = handler;
-        }
-    }
-
-    function detachEventListener(target, eventType, handler) {
-        if (typeof target.removeEventListener != "undefined") {
-            target.removeEventListener(eventType, handler, false);
-        } else if (typeof target.detachEvent != "undefined") {
-            target.detachEvent("on" + eventType, handler);
-        } else {
-            target["on" + eventType] = null;
-        }
-    }
-
-    //去掉字符串首尾空格
-    function trim(str) {
-        if (typeof str !== 'string') {
-            return '';
-        }
-        return str.replace(/^\s*|\s*$/g, '');
-    }
-
     //操作class
     function hasClass(elements, cName) {
         return !!elements.className.match(new RegExp("(\\s|^)" + cName + "(\\s|$)"));
@@ -176,62 +118,6 @@
         }
     }
 
-    //对象属性继承
-    function extend(obj1, obj2) {
-        for (var k in obj2) {
-            obj1[k] = obj2[k];
-        }
-        return obj1;
-    }
-
-    //阻止默认事件
-    function stopDefaultAction(event) {
-        if (event.preventDefault) {
-            event.preventDefault();  //标准
-        } else {
-            event.returnValue = false;  //IE6,7,8
-        }
-    }
-
-    //防止事件冒泡
-    function unPropagation(event) {  //这里的event已经确定是事件对象，不为空了
-        if (event.stopPropagation) {
-            event.stopPropagation();  //标准
-        } else {
-            //event.cancelBubble不能作为判定这个属性有无的表达式，因为event.cancelBubble的默认值就是false
-            event.cancelBubble = true;  //IE6,7,8
-        }
-    }
-
-    //返回视口尺寸（window.innerWidth 包括页面的滚动条，其让方式获得视口宽度都不包括滚动条，媒体查询的尺寸侦听是包括滚动条的）
-    function getViewportSize(w) {
-        w = w || window;
-        //除了IE8和更早的版本以外
-        if (w.innerWidth != null) {
-            return {w: w.innerWidth, h: w.innerHeight};
-        } else {
-            var d = w.document;
-            //对标准模式下的IE（或其他浏览器）
-            if (document.compatMode == "CSS1Compat") {
-                return {w: d.documentElement.clientWidth, h: d.documentElement.clientHeight};
-            } else {
-                //对怪异模式下的浏览器
-                return {w: d.body.clientWidth, h: d.body.clientHeight};
-            }
-        }
-    }
-
-    //获取元素的计算样式
-    function retrieveComputedStyle(element, styleProperty) {
-        var computedStyle = null;
-        if (typeof element.currentStyle != "undefined") {
-            computedStyle = element.currentStyle; //IE6,7,8
-        }
-        else {
-            computedStyle = document.defaultView.getComputedStyle(element, null);  //标准
-        }
-        return computedStyle[styleProperty];
-    }
 
     if (typeof define === 'function') {
         define(function () {
